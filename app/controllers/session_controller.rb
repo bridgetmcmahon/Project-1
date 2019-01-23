8a06ce282339cc09f48ac663e675bc79ef557671
Mossy <bridgo.mcmahon@gmail.com>
2019-01-23 23:36:58 +1100

    ui and styling

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to shelves_path
     else
       flash[:error_message] = "Invalid email or password"
       redirect_to login_path
